c6a74042fa71022494e1fb796371e24fa34aff89
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-01-08 22:20:50 -0600

    added http

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
+import { HttpModule } from '@angular/http'; 
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component'; 
@@ -12,10 +13,16 @@ import { DashboardComponent } from './dashboard.component';
 
 import { AppRoutingModule } from './app-routing.module'; 
 
+// In memory web api
+import { InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service'; 
+
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
    ],
    
